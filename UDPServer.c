1724953a8cc5b82479f2240282278ec20149d7c9
Salizer <rene_thomsen@yahoo.com>
2011-04-05 14:36:09 +0200

    Updated UDP server and client code

--- UDPServer.c
+++ UDPServer.c
@@ -7,11 +7,7 @@
 #include <netdb.h>
 #include <stdio.h>
 
-void error(const char *msg)
-{
-    perror(msg);
-    exit(0);
-}
+#define BUFFSIZE 1024
 
 //Uptime function.
 char *getUpTime()
@@ -41,10 +37,7 @@ char* getLoadAvg()
 
     // Open file
     if((file = fopen("/proc/loadavg", "rt")) == NULL) // Open and check if error
-    {
-        puts("Could not open /proc/loadavg");
-        exit(1);
-    }
+		perror("loadAvg"), exit(EXIT_FAILURE);
 
     // Read file and save in buffer
     bzero(loadAvg_chr, sizeof(loadAvg_chr));
@@ -65,17 +58,21 @@ int main(int argc, char *argv[])
     socklen_t fromlen;
     struct sockaddr_in server; 
     struct sockaddr_in from;
-    char buf[1024];
+    char buf[BUFFSIZE+1];
 
    if (argc < 2) {
-      fprintf(stderr, "ERROR, no port provided\n");
+      fprintf(stderr, "ERROR: No port provided\n");
       exit(0);
    }
    
    /////////////////////////////////////////////
    //Create socket with protocol UDP
    sock=socket(AF_INET, SOCK_DGRAM, 0);
-   if (sock < 0) error("Opening socket");
+   if (sock < 0)
+   {
+        printf("Error: Opening socket.\n");
+        exit(1);
+   }
    
    ////////////////////////////////////////////
    //Theese lines initializes server to zeros.
@@ -85,7 +82,7 @@ int main(int argc, char *argv[])
    ////////////////////////////////////////////
    //Use INTERNET domain (internet sockets).
    server.sin_family=AF_INET;
-   //As server, use own IP
+   //As server, use own IP(From all interfaces)
    server.sin_addr.s_addr=INADDR_ANY;
    //Convert port number to network byte order and save it to structure.
    server.sin_port=htons(atoi(argv[1]));
@@ -93,7 +90,10 @@ int main(int argc, char *argv[])
    ///////////////////////////////////////////////
    //The bind() system calls binds a socket to an address (address of the current host and port number).
    if (bind(sock,(struct sockaddr *)&server,length)<0) 
-       error("binding");
+    {
+		printf("Error: Binding.\n");
+        exit(1);
+    }
        
    fromlen = sizeof(struct sockaddr_in);
    
@@ -105,9 +105,13 @@ int main(int argc, char *argv[])
         
         //Recieve data from client
 		puts("Waiting for client...\n");
-        n = recvfrom(sock,buf,1024,0,(struct sockaddr *)&from,&fromlen);
-        if (n < 0) error("recvfrom");
-       
+        n = recvfrom(sock,buf,BUFFSIZE,0,(struct sockaddr *)&from,&fromlen);
+        if (n < 0)
+		{
+		printf("Error: Recieving.\n");
+		exit(1);
+		}
+		
        //Write what was recieved.
        write(1,"Received a datagram: ",21);
        write(1,buf,n); 
@@ -122,7 +126,11 @@ int main(int argc, char *argv[])
             strcpy(buf, tmpBuf);
             
             n = sendto(sock,buf,strlen(buf) + 1,0,(struct sockaddr *)&from,fromlen); // Add +1 because of 0-termination
-                    if(n  < 0) error("sendto");
+            if (n < 0)
+			{
+				printf("Error: Sending.\n");
+				exit(1);
+			}
                     
             free(tmpBuf); 
         }
@@ -135,7 +143,11 @@ int main(int argc, char *argv[])
             strcpy(buf, tmpBuf);
             
             n = sendto(sock,buf,strlen(buf) + 1, 0,(struct sockaddr *)&from,fromlen);
-                    if(n  < 0) error("sendto");
+			if (n < 0)
+			{
+				printf("Error: Sending.\n");
+				exit(1);
+			}
             
             free(tmpBuf); 
         }
@@ -144,8 +156,7 @@ int main(int argc, char *argv[])
 			strcpy(buf, "Screw you\n");
 			sendto(sock,buf,strlen(buf) + 1, 0,(struct sockaddr *)&from,fromlen);
 		}
-		//if(tmpBuf != NULL)
-		//	free(tmpBuf);  
+
    }
    return 0;
  }
