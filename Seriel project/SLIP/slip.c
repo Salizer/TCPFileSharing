cbdf7a18bb9b75445024729b9c4bd720b0f11f2d
Zalizer <renehthomsen@gmail.com>
2011-04-27 20:05:55 +0200

    Made start for SLIP for DKT

---
+++ Seriel project/SLIP/slip.c
@@ -0,0 +1,127 @@
+#include "slip.h"
+
+// Global defines
+#define FRAME_CHAR 'A' // Start and end of data
+#define FRAME_CHAR_SUB1 'B' // Substitute character 1 in case of FRAME_CHAR occuring in data
+#define FRAME_CHAR_SUB2 'C' // Substitute character 2 in case of FRAME_CHAR occuring in data
+#define SUB_SUB 'D' // If FRAME_CHAR_SUB1 occurs in data
+
+// Global variables
+v24_port_t* currentConnection = 0; // Used to keep track of connection
+
+// Implementation of the functions in "slip.h"
+int connect()
+{
+	// Tmp variables
+	int params = 0;
+	
+	// Open
+	currentConnection = v24OpenPort("/dev/ttyusb0",V24_STANDARD);
+	if(currentConnection == 0) // Error occured
+		return(-1);
+	
+	// Settings
+	/*
+	 *	Baud rate: 9600
+	 * 	Databit: 8 bit
+	 * 	Parity bit generation: Disabled
+	 */
+	params = v24SetParameters(currentConnection,V24_B9600,V24_8BIT,V24_NONE);
+	if(rc!=V24_E_OK) // Error occured
+	{
+		v24ClosePort(UsedPort);
+		return(-1);
+	}
+}
+
+void close()
+{
+	v24ClosePort(UsedPort);
+}
+
+void sendPacket(char* data, size_t n)
+{
+	// Check size
+	if(n > DATA_MAX)
+		return;
+	
+	// Start of package
+	24Putc(currentConnection, FRAME_CHAR);
+	
+	// Send data
+	while(n) // For each byte
+	{
+		// Case of FRAME_CHAR or FRAME_CHAR_SUB1 in data(Else just send)
+		switch(*data)
+		{
+			case FRAME_CHAR:
+				24Putc(currentConnection, FRAME_CHAR_SUB1);
+				24Putc(currentConnection, FRAME_CHAR_SUB2);
+			break;
+			case FRAME_CHAR_SUB1:
+				24Putc(currentConnection, FRAME_CHAR_SUB1);
+				24Putc(currentConnection, SUB_SUB);
+			break;
+			default: // Just send
+				24Putc(currentConnection, *data);
+			break;
+		}
+		// Next byte
+		data++;
+	}
+	
+	// End of package
+	24Putc(currentConnection, FRAME_CHAR);
+}
+
+size_t receivePackage(char data[]);
+{
+	// Remember case of double FRAME_CHAR in start of new package: FRAME_CHAR --DATA-- FRAME_CHAR FRAME_CHAR --DATA-- FRAME_CHAR
+	// Also remember not to give back FRAME_CHAR´s in start and end
+	
+	// Tmp variables
+	char currentChar = 0;
+	char specialChar = 0; // In case of byte stuffing
+	size_t index = 0; // Index for changing 'data'
+	size_t read = 0; // How many bytes read
+	
+	if(v24Getc (currentConnection) != FRAME_CHAR) // Error in data stream
+	{
+		// Ignore until next FRAME_CHAR encountered
+		while(v24Getc (currentConnection) != FRAME_CHAR)
+			{}
+			
+		return(0);
+	}
+	
+	// Get data
+	while((currentChar = v24Getc (currentConnection)) != FRAME_CHAR) // Until end of 'frame' encountered
+	{
+		// Check if special char or just normal
+		if(currentChar == FRAME_CHAR_SUB1)
+		{
+			specialChar = v24Getc (currentConnection); // Get next char
+			switch(specialChar)
+			{
+				case FRAME_CHAR_SUB2:
+					data[index] = FRAME_CHAR;
+				break;
+				case SUB_SUB:
+					data[index] = FRAME_CHAR_SUB1;
+				break;
+				default: // Error occured
+					// Ignore until next FRAME_CHAR encountered
+					while(v24Getc (currentConnection) != FRAME_CHAR)
+						{}
+						
+					return(0);
+				break;
+			}
+		}
+		else // Normal
+			data[index] = currentChar;
+
+		read++;
+	}
+	return(read);
+}
---
+++ Seriel project/SLIP/slip.h
@@ -0,0 +1,44 @@
+#ifndef SLIP_H
+#define SLIP_H
+/** \file slip.h*/
+/** \brief Implements SLIP(Seriel Line Internet Protocol) transfer over serial communication(RS-232).
+ * 			Uses the standard: http://tools.ietf.org/html/rfc1055
+ * 			Except: 
+ * 			<ul>
+ * 				<li> Uses one kind of byte for start and finish('A')
+ * 				<li> Can send a maximum of DATA_MAX characters at a time(Can actually be more because of byte-stuffing)
+ * 			</ul>
+ *	\author NSRD
+ * 	\date 27/04 -11
+ */
+ #include <ezV24/ezV24.h> // Used for serial connection
+
+/** Global constants */
+const DATA_MAX = 1050;
+
+// Functions
+/**	\brief Makes connection over "/dev/ttyS0";.
+*	\pre Another serial connection is not open.(Remember to close after using this connection)
+* 	\pos A connection is made(Returns '1' else '0').
+*/
+int connect();
+ 
+/**	\brief Closes connection.
+*	\pre A serial connection is open.
+* 	\pos The connection is closed.
+*/
+void close();
+ 
+/**	\brief Sends a package.
+*	\pre A connection is open. Data contains minimum 'n' number of bytes. 'n' is less than or equal to DATA_MAX.	
+*	\pos 'n' number of bytes from 'data' is sent.
+*/
+void sendPacket(char* data, size_t n);
+ 
+/** \brief Receives a package.
+*	\pre A connection is open. Data can contain atleast DATA_MAX * 2.
+*	\pos 'data' now contains the data from the package and the number of bytes received is returned.(0 if error)
+*/
+size_t receivePackage(char data[]);
+
+#endif
